f3d793224cbc09ab21ef4832992f2a0b698e733f
Samuel Buriticá <sbsr95@hotmail.com>
2024-10-17 11:06:20 -0500

    lesson 10 recursion

--- lesson10/rps3.py
+++ lesson10/rps3.py
@@ -2,23 +2,22 @@ import sys
 import random
 from enum import Enum
 
-class RPS(Enum):
-    ROCK = 1
-    PAPER = 2
-    SCISSORS = 3
+def play_rps():
+    class RPS(Enum):
+        ROCK = 1
+        PAPER = 2
+        SCISSORS = 3
 
-playagain = True
-
-while playagain:
 
     playerchoice = input(
         "\nEnter... \n1 for Rock, \n2 for Paper, or \n3 for Scissors:\n\n"
     )
 
-    player = int(playerchoice)
+    if playerchoice not in ["1", "2", "3"]:
+        print("You must enter 1, 2, or 3")
+        return play_rps()
 
-    if player < 1 or player > 3:
-        sys.exit("You must enter 1, 2, or 3")
+    player = int(playerchoice)
 
     computerchoice = random.choice("123")
 
@@ -38,12 +37,19 @@ while playagain:
     else:
         print("Python wins!")
 
-    playagain = input("\nPlay again? \nY for Yes or \nQ to Quit: \n\n")
+    print("\nPlay again?")
+
+    while True:
+        playagain = input("\nPlay again? \nY for Yes or \nQ to Quit: \n")
+        if playagain.lower() not in ["y", "q"]:
+            continue
+        else:
+            break
 
     if playagain.lower() == "y":
-        continue
+        return play_rps()
     else:
         print("Thanks for playing.")
-        playagain = False
+        sys.exit("Bye!")
 
-sys.exit("Bye!")
+play_rps()
