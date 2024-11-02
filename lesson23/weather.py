e15af1b7b8835e7418dcba40504f7f911598fd70
Samuel Buriticá <sbsr95@hotmail.com>
2024-11-01 22:17:44 -0500

    weather conditions app with flask

---
+++ lesson23/weather.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+from pprint import pprint
+import requests
+import os
+
+load_dotenv()
+
+def get_current_weather(city="Envigado"):
+    request_url = f'https://api.openweathermap.org/data/2.5/weather?&appid={os.getenv("API_KEY")}&q={city}&units=metric'
+
+    weather_data = requests.get(request_url).json()
+
+    return weather_data
+
+if __name__ == '__main__':
+    print('\n*** Get Current Weather Conditions ***\n')
+
+    city = input('\nPlease enter a city name:\n')
+
+    # Check for empty strings or with only spaces
+    if not bool(city.strip()):
+        city = "Envigado"
+
+    weather_data = get_current_weather(city)
+
+    pprint(weather_data)
